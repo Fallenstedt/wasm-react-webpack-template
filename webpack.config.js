83ebd0bd85be388c69130d1f9719aa186dba8f06
Alexander Fallenstedt <a.fallenstedt@gmail.com>
2020-07-04 18:42:36 -0700

    default to production build (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -5,35 +5,34 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.tsx"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js",
-    chunkFilename: "[name].chunk.js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
+	mode: "production",
+	entry: {
+		index: "./js/index.tsx",
+	},
+	output: {
+		path: dist,
+		filename: "[name].js",
+		chunkFilename: "[name].chunk.js",
+	},
+	devServer: {
+		contentBase: dist,
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loader: "ts-loader" },
+		],
+	},
+	plugins: [
+		new CopyPlugin([path.resolve(__dirname, "static")]),
 
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-    }),
-  ]
+		new WasmPackPlugin({
+			crateDirectory: __dirname,
+			forceMode: "production",
+		}),
+	],
 };
