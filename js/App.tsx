8bec2645b2e4b30867a395a166f54b8372b1e019
Alex Fallenstedt <a.fallenstedt@gmail.com>
2020-12-02 08:44:43 -0800

    refactor

--- js/App.tsx
+++ js/App.tsx
@@ -1,24 +1,60 @@
-import React, { useEffect } from 'react';
-import { Observer } from 'mobx-react'
-import { Greet } from './components/Greet';
-import { useEngines } from './stores/use_engines';
+import React, { useCallback, useEffect, useState } from "react";
+
+type WasmInstance = typeof import("../pkg/index.js");
+
+function useWasm() {
+	const [error, setError] = useState<Error | undefined>(undefined);
+	const [wasmInstance, setWasmInstance] = useState<WasmInstance | undefined>(
+		undefined
+	);
+	const [loading, setLoading] = useState(true);
+	const initialize = useCallback(async () => {
+		try {
+			setLoading(true);
+			setError(undefined);
+			const wasm = await import("../pkg/index.js");
+			setWasmInstance(wasm);
+		} catch (error) {
+			setError(error);
+		} finally {
+			setLoading(false);
+		}
+	}, []);
+
+	return {
+		error,
+		loading,
+		initialize,
+		wasmInstance,
+	};
+}
 
 function App() {
-  const { wasmEngine } = useEngines()
+	// const { wasmEngine } = useEngines()
+	const { error, loading, initialize, wasmInstance } = useWasm();
 
-  useEffect(() => {
-    async function loadWasm() {
-      await wasmEngine.initialize()
-    }
-    loadWasm()
-  }, [])
+	useEffect(() => {
+		async function loadWasm() {
+			await initialize();
+		}
+		loadWasm();
+	}, []);
 
-  return (
-    <Observer>
-      {() => wasmEngine.loading ? <h1>Loading...</h1> : <Greet />}
-    </Observer>
-  )
+	if (error) {
+		console.error(error);
+		return <p>Something went wrong.</p>;
+	}
 
+	if (loading) {
+		return <p>Loading wasm.</p>;
+	}
+
+	if (wasmInstance) {
+		wasmInstance.greet();
+		return <p>You have 1 new message in your console.</p>;
+	}
+
+	return <div></div>;
 }
 
 export default App;
--- js/stores/use_engines.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { WasmEngine } from './WasmEngine';
-
-const globalContext = React.createContext({
-  wasmEngine: new WasmEngine()
-});
-
-export const useEngines = () => React.useContext(globalContext);
-
